d40f8521a49eb073605435dd3cf04d701263d6b6
Hamzah-AL-Ghouli <abduljalilhamzah43@gmail.com>
2025-12-08 21:37:48 +0300

    إضافة نافذة تسجيل الدخول (fm_login) ومعالجة عمليات التحقق من بيانات المستخدم
    إضافة نافذة تسجيل الدخول (fm_login) ومعالجة عمليات التحقق من بيانات المستخدم
    
    - ربط الواجهة بمنطق الأعمال عبر كائن cls_Login.
    - تنفيذ عملية تسجيل الدخول والتحقق من اسم المستخدم وكلمة المرور.
    - عرض رسائل توضيحية للمستخدم في حال نجاح أو فشل تسجيل الدخول.
    - تفعيل عناصر القائمة حسب نوع المستخدم (مدير / موظف).
    - إضافة وظائف مساعدة مثل مسح الحقول والتنقل باستخدام زر Enter.
    - تحسين تجربة المستخدم بجعل مؤشر الكتابة يبدأ في حقل اسم المستخدم تلقائياً.

---
+++ PL/fm_login.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarket.PL
+{
+    public partial class fm_login : Form
+    {
+        BL.cls_Login log = new BL.cls_Login();
+        public fm_login()
+        {
+            InitializeComponent();
+            txt_userName.Focus();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            DataTable dt = log.Login(txt_userName.Text, txt_password.Text);
+           
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("تم تسجيل الدخول بنجاح ", "تسجيل الدخول", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Program.user_name = dt.Rows[0]["Full_name"].ToString();
+                if (dt.Rows[0][2].ToString() == "مدير")
+                {
+                    fm_Main.getMainForm.Enable_menu_strip_for_manager();
+                }
+
+                else
+                    fm_Main.getMainForm.Enable_menu_strip_for_worker();
+
+
+            }
+            else
+             MessageBox.Show("قد يكون هناك خطأ في إسم المستخدم أو كلمة المرور ");
+            this.Close();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            txt_userName.Clear();
+            txt_password.Clear();
+        }
+
+        private void txt_userName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_password.Focus();
+                e.SuppressKeyPress = true;
+            }
+              
+        }
+
+        private void txt_password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_login.Focus();
+                e.SuppressKeyPress = true;
+            }
+
+        }
+    }
+}

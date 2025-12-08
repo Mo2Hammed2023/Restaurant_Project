0d8a4d217d53faddffa8faefe0b6647a8831607a
Hamzah-AL-Ghouli <abduljalilhamzah43@gmail.com>
2025-12-08 21:49:24 +0300

    إضافة نموذج قائمة المنتجات fm_products_list مع تنسيق الجدول وجلب جميع المنتجات
    إضافة نموذج قائمة المنتجات fm_products_list مع تنسيق الجدول وجلب جميع المنتجات
    
    - إنشاء نموذج fm_products_list لعرض قائمة كاملة بالمنتجات.
    - ربط DataGridView مع الإجراء المخزن get_All_product لجلب جميع المنتجات من قاعدة البيانات.
    - تحسين مظهر DataGridView (ارتفاع الصفوف، ألوان الصفوف البديلة، ألوان التحديد).
    - إضافة ميزة إغلاق النموذج عند النقر المزدوج على أحد الصفوف.

---
+++ PL/product/fm_products_list.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SuperMarket.PL
+{
+    partial class fm_products_list
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv_products_list = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_products_list)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_products_list
+            // 
+            this.dgv_products_list.AllowUserToAddRows = false;
+            this.dgv_products_list.AllowUserToDeleteRows = false;
+            this.dgv_products_list.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_products_list.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_products_list.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_products_list.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 14F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_products_list.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_products_list.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_products_list.GridColor = System.Drawing.Color.LightGray;
+            this.dgv_products_list.Location = new System.Drawing.Point(0, 0);
+            this.dgv_products_list.MultiSelect = false;
+            this.dgv_products_list.Name = "dgv_products_list";
+            this.dgv_products_list.ReadOnly = true;
+            this.dgv_products_list.RowHeadersWidth = 51;
+            this.dgv_products_list.RowTemplate.Height = 26;
+            this.dgv_products_list.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_products_list.Size = new System.Drawing.Size(994, 641);
+            this.dgv_products_list.TabIndex = 0;
+            this.dgv_products_list.DoubleClick += new System.EventHandler(this.dgv_products_list_DoubleClick);
+            // 
+            // fm_products_list
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 641);
+            this.Controls.Add(this.dgv_products_list);
+            this.MinimizeBox = false;
+            this.Name = "fm_products_list";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "قائمة جميع المنتجات";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_products_list)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView dgv_products_list;
+    }
+}
---
+++ PL/product/fm_products_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarket.PL
+{
+    public partial class fm_products_list : Form
+    {
+        public fm_products_list()
+        {
+            InitializeComponent();
+            dgv_products_list.DataSource = Program.dal.select("get_All_product", null);
+
+            dgv_products_list.RowTemplate.Height = 35;
+            dgv_products_list.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(245, 245, 245);
+            dgv_products_list.GridColor = Color.LightGray;
+            dgv_products_list.DefaultCellStyle.SelectionBackColor = Color.FromArgb(41, 128, 185);
+            dgv_products_list.DefaultCellStyle.SelectionForeColor = Color.White;
+        }
+
+        private void dgv_products_list_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

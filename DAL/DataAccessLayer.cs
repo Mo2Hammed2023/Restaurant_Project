22c7969fbd0564160af32e7223800176d82ae44a
Mohammed Hamid <154968967+Mo2Hammed2023@users.noreply.github.com>
2025-12-08 20:17:59 +0300

    Add files via upload
    إضافة طبقة الوصول للبيانات (DataAccessLayer) لإدارة الاتصال بقاعدة بيانات سوبرماركت
    
    - إنشاء الاتصال بقاعدة البيانات باستخدام SqlConnection.
    - تنفيذ الإجراءات المخزنة (Stored Procedures) للقراءة (select) والتنفيذ (Execute).
    - دعم تمرير المعاملات للإجراءات المخزنة باستخدام SqlParameter.
    - إدارة فتح وإغلاق الاتصال بشكل آمن.

---
+++ DAL/DataAccessLayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+
+namespace SuperMarket.DAL
+{
+    internal class DataAccessLayer
+    {
+        SqlConnection sqlCon;
+        SqlCommand cmd;
+
+        //This constructor inisialize the connection object
+        public DataAccessLayer()
+        {
+            sqlCon = new SqlConnection(
+                @"Server =.\SQLEXPRESS;
+                Database=SuperMarket-db;
+                Integrated Security=true");
+        }
+
+
+        public DataTable select(string stored_procedure, SqlParameter[] param)
+        {
+            sqlCon.Open();
+            using (cmd = new SqlCommand(stored_procedure, sqlCon))
+            {
+                helper(param);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                sqlCon.Close();
+                return dt;
+            }
+        }
+
+        public void helper(SqlParameter[] param)
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (param != null)
+                cmd.Parameters.AddRange(param);
+        }
+
+
+        public void Execute(string stored_procedure, SqlParameter[] param)
+        {
+            sqlCon.Open();
+            using (cmd = new SqlCommand(stored_procedure, sqlCon))
+            {
+                helper(param);
+                cmd.ExecuteNonQuery();
+                sqlCon.Close();
+            }
+        }
+
+
+      
+
+    }
+}
